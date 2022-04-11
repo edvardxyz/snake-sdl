30eee9f7ed388c8113f936fd1e0040da9d4c74e1
edvardxyz <71638987+edvardxyz@users.noreply.github.com>
2022-04-11 16:25:23 +0200

    :star:

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#define WIDTH 640
-#define HEIGHT 480
-#define SIZE 15
-#define FPS 1
+
+#define WIDTH 600
+#define HEIGHT 600
+#define SIZE 10
 
 enum direction
 {
@@ -23,10 +24,11 @@ struct Node
 struct Apple
 {
   SDL_Rect rect;
+  int score;
   struct Node *next;
 };
 
-struct Node* MoveSnake(struct Node*, SDL_Rect);
+struct Node* MoveSnake(struct Node*, SDL_Rect, bool);
 
 int main(int argc, char *argv[])
 {
@@ -47,8 +49,7 @@ int main(int argc, char *argv[])
     return 0;
   }
   // Create a renderer
-  Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
-  SDL_Renderer *rend = SDL_CreateRenderer(wind, -1, render_flags);
+  SDL_Renderer *rend = SDL_CreateRenderer(wind, -1, 0);
   if (!rend)
   {
     printf("Error creating renderer: %s\n", SDL_GetError());
@@ -57,16 +58,17 @@ int main(int argc, char *argv[])
     return 0;
   }
 
-  // Main loop
-  bool running = true;
+  // Init game vars and snake body
   int size = 0;
   int score = 0; 
   int dir = RIGHT;
-  SDL_Rect head = {WIDTH / 2, HEIGHT / 2, SIZE, SIZE};
-  SDL_Event event;
+  int FPS = 3;
 
+  // create snake with 3 segments
+  SDL_Rect head = {WIDTH / 2, HEIGHT / 2, SIZE, SIZE};
   struct Node butt;
   struct Node body;
+  // Node pointer to keep track of butt of snake
   struct Node *p_butt = &butt;
 
   butt.rect.h = SIZE;
@@ -81,8 +83,26 @@ int main(int argc, char *argv[])
   body.rect.x = butt.rect.x + SIZE;
   body.next = NULL;
 
+  // Create apple
+
+  struct Apple apple;
+  apple.rect.h = SIZE;
+  apple.rect.w = SIZE;
+  apple.rect.x = rand() % 60 * SIZE;
+  apple.rect.y = rand() % 60 * SIZE;
+  apple.score = 5;
+  apple.next = NULL;
+
+  bool eaten = false;
+
+  // Main loop
+  SDL_Event event;
+  bool running = true;
+  bool pressed = false;
+
   while (running)
   {
+    pressed = false;
     // Process events
     while (SDL_PollEvent(&event))
     {
@@ -92,34 +112,39 @@ int main(int argc, char *argv[])
         running = false;
         break;
       case SDL_KEYDOWN:
-        switch (event.key.keysym.scancode)
+
+        if (!pressed)
         {
-        case SDL_SCANCODE_A:
-        case SDL_SCANCODE_LEFT:
-        case SDL_SCANCODE_H:
-          if (dir != RIGHT)
-            dir = LEFT;
-          break;
-        case SDL_SCANCODE_D:
-        case SDL_SCANCODE_RIGHT:
-        case SDL_SCANCODE_L:
-          if (dir != LEFT)
-            dir = RIGHT;
-          break;
-        case SDL_SCANCODE_W:
-        case SDL_SCANCODE_UP:
-        case SDL_SCANCODE_K:
-          if (dir != DOWN)
-            dir = UP;
-          break;
-        case SDL_SCANCODE_S:
-        case SDL_SCANCODE_DOWN:
-        case SDL_SCANCODE_J:
-          if (dir != UP)
-            dir = DOWN;
-          break;
-        default:
-          break;
+          switch (event.key.keysym.scancode)
+          {
+          case SDL_SCANCODE_A:
+          case SDL_SCANCODE_LEFT:
+          case SDL_SCANCODE_H:
+            if (dir != RIGHT)
+              dir = LEFT;
+            break;
+          case SDL_SCANCODE_D:
+          case SDL_SCANCODE_RIGHT:
+          case SDL_SCANCODE_L:
+            if (dir != LEFT)
+              dir = RIGHT;
+            break;
+          case SDL_SCANCODE_W:
+          case SDL_SCANCODE_UP:
+          case SDL_SCANCODE_K:
+            if (dir != DOWN)
+              dir = UP;
+            break;
+          case SDL_SCANCODE_S:
+          case SDL_SCANCODE_DOWN:
+          case SDL_SCANCODE_J:
+            if (dir != UP)
+              dir = DOWN;
+            break;
+          default:
+            break;
+          }
+          pressed = true;
         }
         break;
       default:
@@ -127,8 +152,44 @@ int main(int argc, char *argv[])
       }
     }
 
-    // Move Snake
-    p_butt = MoveSnake(p_butt, head);
+    // check if collision with apple
+    if (apple.rect.y == head.y && apple.rect.x == head.x)
+    {
+      eaten = true;
+      score += apple.score;
+      // TODO not inside snake
+      apple.rect.x = rand() % 60 * SIZE;
+      apple.rect.y = rand() % 60 * SIZE;
+      apple.score = rand() % 20;
+      if(apple.score > 10){
+        FPS += 5;
+      }else{
+        FPS += 1;
+      }
+    }
+
+    // check collision with self
+    struct Node *p = p_butt;
+    while(p != NULL){
+
+      if(p->rect.x == head.x && p->rect.y == head.y){
+        // TODO save score if highest
+        // restart game 
+        exit(1);
+      }
+      p = p->next;
+    }
+
+
+    // check collision with edge 
+    if(head.x >= WIDTH || head.x < 0 || head.y >= HEIGHT || head.y < 0){
+      exit(1);
+    }
+
+
+    p_butt = MoveSnake(p_butt, head, eaten);
+
+    eaten = false;
 
     // Move snake in direction of dir
     switch (dir)
@@ -151,11 +212,20 @@ int main(int argc, char *argv[])
     }
 
 
+    // print score in window title
+    char score_str[256];
+    sprintf(score_str, "Snake | Score: %d | Highscore: TODO | FPS: %d", score, FPS);
+    SDL_SetWindowTitle(wind, score_str);
+
+
     // Clear screen
     SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
     SDL_RenderClear(rend);
 
 
+    // Draw the apple
+    SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+    SDL_RenderFillRect(rend, &apple.rect);
 
     // NEEDS to be before body drawing
     // Draw the head
@@ -182,17 +252,31 @@ int main(int argc, char *argv[])
 
 // Moves snake by moving butt node to front of node list
 // Then returns pointer to new butt of snake
-struct Node* MoveSnake(struct Node* butt, SDL_Rect head){
+struct Node *MoveSnake(struct Node *butt, SDL_Rect head, bool eaten)
+{
 
-  // Allocate for new node
-  //struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+  struct Node *p = butt;
 
   // make nodehead point to back as to back Node is going to become new nodehead
-  struct Node *p = butt;
   do
   {
     if (p->next == NULL)
     {
+      if (eaten)
+      {
+        // create new node with same pos as head
+        struct Node *new_segment = (struct Node *)malloc(sizeof(struct Node));
+        if(new_segment == NULL){
+          printf("Failed to allocate new node\n");
+          exit(-1);
+        }
+        new_segment->next = NULL;
+        new_segment->rect = head;
+        // make last body part point to new segment
+        p->next = new_segment;
+        return butt;
+      }
+
       p->next = butt;
       break;
     }
