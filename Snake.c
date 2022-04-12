ad45c4510084f354027b9d956587fa73c4d20878
edvardxyz <71638987+edvardxyz@users.noreply.github.com>
2022-04-12 16:27:47 +0200

    sounds added

--- Snake.c
+++ Snake.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
-#define WIDTH 600
-#define HEIGHT 600
+#define WIDTH 420
+#define HEIGHT 300
 #define SIZE 20
+
+#define NUM_WAVE 2
+const char* _waveFileNames[] = 
+{
+  "eat.wav",
+  "die.wav"
+};
+
+Mix_Chunk* _sample[2];
 
 enum direction
 {
@@ -28,6 +38,37 @@ typedef struct Apple
 } Apple;
 
 Node* MoveSnake(Node*, SDL_Rect, bool);
+
+int Init(void)
+{
+  memset(_sample, 0, sizeof(Mix_Chunk *) * 2);
+
+  // Set up the audio stream
+  int result = Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 512);
+  if (result < 0)
+  {
+    fprintf(stderr, "Unable to open audio: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  result = Mix_AllocateChannels(4);
+  if (result < 0)
+  {
+    fprintf(stderr, "Unable to allocate mixing channels %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  // Load waveforms
+  for (int i = 0; i < NUM_WAVE; i++)
+  {
+    _sample[i] = Mix_LoadWAV(_waveFileNames[i]);
+    if (_sample[i] == NULL)
+    {
+      fprintf(stderr, "Unable to load wave file: %s Error: %s\n", _waveFileNames[i], Mix_GetError());
+    }
+  }
+  return true;
+}
 
 int main(int argc, char *argv[])
 {
@@ -56,23 +97,29 @@ int main(int argc, char *argv[])
     SDL_Quit();
     return 0;
   }
-
+  
+  if ( Init() == false){
+    return 1;
+  }
 
   // Init game vars
-  int score = 0; 
+  int score = 0;
   int dir = RIGHT;
-  int FPS = 10;
+  int FPS = 8;
   int highscore = 0;
+  int divX = WIDTH / SIZE;
+  int divY = HEIGHT / SIZE;
 
   // Get highscore
   FILE *fptr = fopen("highscore.dat", "r");
-  if(fptr != NULL) {
+  if (fptr != NULL)
+  {
     fscanf(fptr, "%d", &highscore);
   }
   fclose(fptr);
 
   // create snake with 3 segments
-  SDL_Rect head = {WIDTH / 2, HEIGHT / 2, SIZE, SIZE};
+  SDL_Rect head = {(WIDTH / 2) + ((WIDTH / 2) % SIZE), (HEIGHT / 2) + ((HEIGHT / 2) % SIZE), SIZE, SIZE};
   Node butt;
   Node body;
   // Node pointer to keep track of butt of snake
@@ -95,11 +142,12 @@ int main(int argc, char *argv[])
   struct Apple apple;
   apple.rect.h = SIZE;
   apple.rect.w = SIZE;
-  apple.rect.x = rand() % 30 * SIZE;
-  apple.rect.y = rand() % 30 * SIZE;
+  apple.rect.x = rand() % divX * SIZE;
+  apple.rect.y = rand() % divY * SIZE;
   apple.next = NULL;
 
   bool eaten = false;
+  bool dead = false;
 
   // Main loop
   SDL_Event event;
@@ -161,31 +209,42 @@ int main(int argc, char *argv[])
     // check if collision with apple
     if (apple.rect.y == head.y && apple.rect.x == head.x)
     {
+      Mix_PlayChannel(-1, _sample[0], 0);
       eaten = true;
       score += 5;
       // TODO not inside snake
-      apple.rect.x = rand() % 30 * SIZE;
-      apple.rect.y = rand() % 30 * SIZE;
+      apple.rect.x = rand() % divX * SIZE;
+      apple.rect.y = rand() % divY * SIZE;
     }
 
     // check collision with self
     Node *p = p_butt;
-    while(p != NULL){
+    while (p != NULL)
+    {
 
-      if(p->rect.x == head.x && p->rect.y == head.y){
+      if (p->rect.x == head.x && p->rect.y == head.y)
+      {
         // TODO save score if highest
-        // restart game 
-        running = false;
+        // restart game
+        dead = true;
       }
       p = p->next;
     }
 
-
-    // Check collision with edge 
-    if(head.x >= WIDTH || head.x < 0 || head.y >= HEIGHT || head.y < 0){
-        running = false;
+    // Check collision with edge
+    if (head.x >= WIDTH || head.x < 0 || head.y < 0 || head.y >= HEIGHT)
+    {
+      dead = true;
     }
 
+    if (dead){
+      running = false;
+      Mix_PlayChannel(-1, _sample[1], 0);
+      while(Mix_Playing(-1) !=0){
+        SDL_Delay(200);
+      }
+      break;
+    }
 
     p_butt = MoveSnake(p_butt, head, eaten);
 
@@ -211,7 +270,6 @@ int main(int argc, char *argv[])
       break;
     }
 
-
     // Print game info in window title
     char score_str[256];
     sprintf(score_str, "Snake | Score: %d | Highscore: %d", score, highscore);
@@ -232,7 +290,8 @@ int main(int argc, char *argv[])
 
     // Draw body segments of snake
     Node *segment = p_butt;
-    while(segment != NULL){
+    while (segment != NULL)
+    {
       SDL_RenderFillRect(rend, &segment->rect);
       segment = segment->next;
     }
@@ -243,15 +302,21 @@ int main(int argc, char *argv[])
   }
 
   // Save score if bigger than highscore
-  if(score > highscore){
-  FILE *fptr = fopen("highscore.dat", "w");
-    if(fptr != NULL){
+  if (score > highscore)
+  {
+    FILE *fptr = fopen("highscore.dat", "w");
+    if (fptr != NULL)
+    {
       fprintf(fptr, "%d", score);
     }
     fclose(fptr);
   }
 
   // Release resources
+  for ( int i = 0; i < NUM_WAVE; i++){
+    Mix_FreeChunk(_sample[i]);
+  }
+  Mix_CloseAudio();
   SDL_DestroyRenderer(rend);
   SDL_DestroyWindow(wind);
   SDL_Quit();
@@ -274,7 +339,8 @@ Node *MoveSnake(Node *butt, SDL_Rect head, bool eaten)
       {
         // create new node with same pos as head
         Node *new_segment = (Node *)malloc(sizeof(Node));
-        if(new_segment == NULL){
+        if (new_segment == NULL)
+        {
           printf("Failed to allocate new node\n");
           exit(1);
         }
